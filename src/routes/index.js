bd75d8a79fcb86c69d1656718c7a8dd401095271
Pablo Smolak <pablosmolak2004@hotmail.com>
2024-02-14 14:49:56 -0400

    ajustando api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,12 @@
+import logRoutes from "../middlewares/LogRoutesMiddleware.js";
 import uploads from "../routes/imageRouter.js"
 
 const routes = (app) => {
+
+    if (process.env.DEBUGLOG === "true") {
+        app.use(logRoutes);
+    }
+
     app.route("/").get((req, res) => {
         res.status(200).redirect("/docs")
     });
