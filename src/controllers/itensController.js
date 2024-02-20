ba98900a234997291f107bf01ae2c467ae561839
Luccas <luccas.rossarolla@gmail.com>
2024-02-20 19:23:24 -0400

    cadastrar itens

--- src/controllers/itensController.js
+++ src/controllers/itensController.js
@@ -1,7 +1,29 @@
 // controller alterado por Luccas
+import Item from "../models/Item.js"; // Corrigido aqui
 
-class itensController {
+export default class itensController {
+  static CadastrarItens = async (req, res) => {
+    try {
+      const { nome, estado, descricao, inventario, setor, auditor, imagem } =
+        req.body;
 
-    static AdicionarItens = async (req, res) => {
-        
+      const item = new Item({
+        nome,
+        estado,
+        descricao,
+        inventario,
+        setor,
+        auditor,
+        imagem,
+      });
+
+      await item.save();
+      res.status(200).json({ item, message: "Item cadastrado com sucesso" });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ error: true, code: 500, message: "Erro interno no servidor" });
+    }
+  };
 }
