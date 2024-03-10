9ba2eba5e3fb54a76cb4a86b6ae7d0e6f891e1e0
gustavo <duarte.guga2025@gmail.com>
2024-03-10 18:11:04 -0400

    Incluindo as auth em todas as rotas necessarias

--- src/routes/campusRouter.js
+++ src/routes/campusRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import CampusController from "../controllers/CampusController.js";
 import ValidateCampus from "../services/validation/campusValidation.js";
+import { AuthMiddleware } from "../middlewares/AuthMiddleware.js";
 
 const router = express.Router();
 
 router
-    .get("/campus", CampusController.pesquisarCampus)
-    .get("/capus/:id", CampusController.listarCampusID)
-    .post("/campus", ValidateCampus.validateCriar, CampusController.criarCampus)
-    .delete("/campus/:id", CampusController.deletarCampus);
+    .get("/campus", AuthMiddleware, CampusController.pesquisarCampus)
+    .get("/capus/:id", AuthMiddleware, CampusController.listarCampusID)
+    .post("/campus", AuthMiddleware, ValidateCampus.validateCriar, CampusController.criarCampus)
+    .delete("/campus/:id", AuthMiddleware, CampusController.deletarCampus);
 
 export default router;
--- tests/routes/routeCampus.spec.js
+++ tests/routes/routeCampus.spec.js
@@ -1,5 +1,5 @@
 
-import {describe, expect, it} from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
 import request from "supertest";
 import app from "../../src/app.js";
 import messages from "../../src/utils/mensagens.js";
@@ -13,15 +13,33 @@ const exemplo_campus = {
 
 };
 
-
 describe("Campus", () => {
     const req = request(app);
     let campusID;
+    let token;
+
+    const userlogin = {
+        email: "dev@gmail.com",
+        senha: "Dev@1234"
+    };
+
+    // eslint-disable-next-line no-undef
+    it("Deve autenticar o usuário e retornar um token", async () => {
+        const resposta = await request(app)
+            .post("/login")
+            .send(userlogin)
+            .set("Accept", "aplication/json")
+            .expect(200);
+
+        expect(resposta.body.token).toBeDefined();
+        return token = resposta.body.token;
+    });
 
     it("Deve retornar uma lista de campus", async () => {
         const dados = await req
             .get("/campus")
             .set("Accept", "aplication/json")
+            .set("Authorization", `Bearer ${token}`)
             .expect(200);
 
         expect(dados.body.message).toEqual(messages.httpCodes[200]);
@@ -32,20 +50,19 @@ describe("Campus", () => {
             .post("/campus")
             .send(exemplo_campus)
             .set("Accept", "application/json")
+            .set("Authorization", `Bearer ${token}`)
             .expect(201);
-
-            console.log(resposta.body);
 
         expect(resposta.body.message).toContain(messages.httpCodes[201]);
 
         campusID = resposta.body.campus._id;
-        console.log(resposta.body.campus._id);
     });
 
     it("Deve deletar um campus", async () => {
         const resposta = await req
             .delete(`/campus/${campusID}`)
             .set("Accept", "application/json")
+            .set("Authorization", `Bearer ${token}`)
             .expect(200);
 
         expect(resposta.body.message).toContain(messages.httpCodes[200]);
