2be601187d44713f416a0af5b4c44e0bf686a58c
estelaferrari <estela.ferrari.2301@gmail.com>
2024-04-02 21:49:52 -0400

    Resolvendo conflito antes do merge

--- src/docs/paths/itensPath.js
+++ src/docs/paths/itensPath.js
@@ -3,6 +3,138 @@ import messages from "../../utils/mensagens.js";
 
 export const itensPaths = {
   "/itens": {
+    get: {
+      tags: ["Itens"],
+      summary: "Listar itens",
+      description:
+        "Retorna uma lista de todos os itens cadastrados no inventário.",
+      parameters: [
+        {
+          name: "pagina",
+          in: "query",
+          description: "Pagina",
+          required: false,
+          schema: {
+            type: "integer"
+          }
+        },
+        {
+          name: "etiqueta",
+          in: "query",
+          description: "Etiqueta de item",
+          required: false,
+          schema: {
+            type: "number"
+          }
+        },
+        {
+          name: "nao_etiquetado",
+          in: "query",
+          description: "Item não etiquetado",
+          required: false,
+          schema: {
+            type: "boolean"
+          }
+        },
+        {
+          name: "encontrado",
+          in: "query",
+          description: "Se ele foi encontrado ou não",
+          required: false,
+          schema: {
+            type: "boolean"
+          }
+        },
+        {
+          name: "nome",
+          in: "query",
+          description: "Nome item",
+          required: false,
+          schema: {
+            type: "string"
+          }
+        },
+        {
+          name: "estado",
+          in: "query",
+          description: "Estado do item",
+          required: false,
+          schema: {
+            type: "string"
+          }
+        },
+        {
+          name: "ativo",
+          in: "query",
+          description: "Se o item esta ativo ou não",
+          required: false,
+          schema: {
+            type: "string"
+          }
+        },
+        {
+          name: "ocioso",
+          in: "query",
+          description: "Se ele esta sendo usado",
+          required: false,
+          schema: {
+            type: "boolean"
+          }
+        },
+        {
+          name: "inventario",
+          in: "query",
+          description: "Inventario que está o item",
+          required: false,
+          schema: {
+            type: "ID"
+          }
+        },
+        {
+          name: "setor",
+          in: "query",
+          description: "Setor do Item",
+          required: false,
+          schema: {
+            type: "ID"
+          }
+        },
+        {
+          name: "auditor",
+          in: "query",
+          description: "Auditor do Item",
+          required: false,
+          schema: {
+            type: "ID"
+          }
+        },
+        {
+          name: "responsavel",
+          in: "query",
+          description: "Responsavel pelo item",
+          required: false,
+          schema: {
+            type: "ID"
+          }
+        },
+      ],
+      responses: {
+        200: {
+          description: messages.httpCodes[200],
+          content: {
+            "application/json": {
+              schema: {
+                $ref: "#/components/schemas/Itens",
+              }
+            }
+          }
+        },
+        500: {
+          description: messages.httpCodes[500],
+        }
+      }
+    },
+
     post: {
       tags: ["Itens"],
       summary: "Cadastrar Item",
@@ -67,8 +199,6 @@ export const itensPaths = {
         }
       }
     },
-  },
-  "/itens/{id}": {
     patch: {
       tags: ["Itens"],
       summary: "Atualizar Item",
@@ -110,7 +240,7 @@ export const itensPaths = {
         }
       }
     },
-  },
+
     delete: {
       tags: ["Itens"],
       summary: "Deletar Item",
@@ -134,9 +264,9 @@ export const itensPaths = {
           description: messages.httpCodes[404]
         },
         500: {
-          description: "Erro interno no servidor.",
-        },
-      },
-    },
-  
+          description: messages.httpCodes[500],
+        }
+      }
+    }
+  }
 };
