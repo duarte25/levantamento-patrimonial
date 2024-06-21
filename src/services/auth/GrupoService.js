d8209bc9b0f4c303c3d2bff2a1c2087154e4c157
gustavo <duarte.guga2025@gmail.com>
2024-06-21 12:52:02 -0400

    Alterando sistemas de permissão commit para realizar isso na faculdade, trocarei de PC

--- src/services/auth/GrupoService.js
+++ src/services/auth/GrupoService.js
@@ -15,27 +15,27 @@ export default class GrupoService {
     as permissões de um usuário */
     static async inicializarGrupos() {
         let listaGrupos = await Grupo.find({});
-        if(!(listaGrupos.length > 0)) {
-            if(process.env.DEBUGLOG === "true") {
+        if (!(listaGrupos.length > 0)) {
+            if (process.env.DEBUGLOG === "true") {
                 console.log("Nenhum grupo no banco, recriando grupos padrão...");
             }
 
             await recriarGruposPadrao();
-            
+
             listaGrupos = await Grupo.find({});
         }
 
         // Transforma a lista de grupos retornada do banco em um objeto para ficar mais fácil de verificar
         //grupos = {};
-        for(let grupo of listaGrupos) {
+        for (let grupo of listaGrupos) {
             const permissoes = {
                 _nivel: grupo.nivel,
                 _oculto: grupo.oculto,
                 _id: grupo._id
             };
-            for(let permissao of grupo.permissoes) {
+            for (let permissao of grupo.permissoes) {
                 let acoes = {};
-                if(permissao.acoes && Array.isArray(permissao.acoes)) for(let acao of permissao.acoes) {
+                if (permissao.acoes && Array.isArray(permissao.acoes)) for (let acao of permissao.acoes) {
                     acoes[acao.nome] = acao;
                 }
                 permissoes[permissao.nome] = acoes;
@@ -45,7 +45,7 @@ export default class GrupoService {
 
         //if(process.env.DEBUGLOG === "true")
         //{
-            //console.log(grupos);
+        //console.log(grupos);
         //}
     }
 
@@ -66,20 +66,20 @@ export default class GrupoService {
     static possuiPermissao(usuarioGrupos, qualPermissao, qualAcao = false, modificadores = false) {
         // Caso a variável de ambiente esteja definida
         // A autorização por permissões é desativada E TODO MUNDO PODE TUDO. 
-        if(process.env.DISABLE_PERMISSION === "true") {
+        if (process.env.DISABLE_PERMISSION === "true") {
             return true;
         }
 
         // se não tem nenhum grupo é VISITANTE
-        if(!usuarioGrupos || !(usuarioGrupos.length > 0)) {
+        if (!usuarioGrupos || !(usuarioGrupos.length > 0)) {
             usuarioGrupos = [GRUPO.VISITANTE];
         }
 
-        for(let nomeGrupo of usuarioGrupos) {
+        for (let nomeGrupo of usuarioGrupos) {
             let grupo = grupos[nomeGrupo];
-            if(!grupo) {
+            if (!grupo) {
                 // Grupo não encontrado, continua para ver os outros grupos do usuario
-                console.error("Problema ao verificar permissão, Usuário com grupo inválido: '"+nomeGrupo+"', ignorando...");
+                console.error("Problema ao verificar permissão, Usuário com grupo inválido: '" + nomeGrupo + "', ignorando...");
                 continue;
             }
 
@@ -87,26 +87,26 @@ export default class GrupoService {
             let permissao = grupo[qualPermissao];
 
             // Este grupo não possui esta permissão, mas outros podem ter, continua procurando
-            if(!permissao) continue;
+            if (!permissao) continue;
 
             // Quando não é especificado uma ação (colocando false em qualAcao) é porque só quer saber se possui a permissão
             // sem saber qual ação específica.
-            if(!qualAcao) return true;
+            if (!qualAcao) return true;
 
             // se possui a ação TUDO é porque pode tudo, inclusive esta ação seja qual ela for
-            if(ACAO.TUDO in permissao) return true;
+            if (ACAO.TUDO in permissao) return true;
 
             // Verifica se possui esta ação, se não possuir, continua procurando
             let acao = permissao[qualAcao];
-            if(!acao) continue;
+            if (!acao) continue;
 
             // Se não precisa checagem de modificadores, retorna verdadeiro já
-            if(!modificadores) return true;
+            if (!modificadores) return true;
 
             // Chegou até aqui é porque precisa de modificadores, verifica se possui todos os necessários
-            if(modificadores.qualquer_usuario && acao.qualquer_usuario !== true) continue;
-            if(modificadores.qualquer_secretaria && acao.qualquer_secretaria !== true) continue;
-            if(modificadores.qualquer_grupo && acao.qualquer_grupo !== true) continue;
+            if (modificadores.qualquer_usuario && acao.qualquer_usuario !== true) continue;
+            if (modificadores.qualquer_secretaria && acao.qualquer_secretaria !== true) continue;
+            if (modificadores.qualquer_grupo && acao.qualquer_grupo !== true) continue;
 
             // Se passou por todos os modificadores, retorna verdadeiro
             return true;
@@ -119,16 +119,16 @@ export default class GrupoService {
     // Utilizado ao atualizar grupos de um usuário
     static validarGrupos(opcoes = {}) {
         return async (value, val) => {
-            if(!value || !Array.isArray(value)) return "O valor em grupos deve ser um array";
-            if(value.length <= 0) return "É necessário especificar pelo menos um grupo";          
+            if (!value || !Array.isArray(value)) return "O valor em grupos deve ser um array";
+            if (value.length <= 0) return "É necessário especificar pelo menos um grupo";
 
-            for(let grupo of value) {
-                if(!grupo || typeof grupo !== "string") {
+            for (let grupo of value) {
+                if (!grupo || typeof grupo !== "string") {
                     return "Especificou um grupo com texto vazio ou inválido";
                 }
 
-                if(!grupos[grupo] || grupos[grupo]._oculto) {
-                    return "O grupo "+grupo+" não existe";
+                if (!grupos[grupo] || grupos[grupo]._oculto) {
+                    return "O grupo " + grupo + " não existe";
                 }
             }
 
@@ -138,31 +138,31 @@ export default class GrupoService {
 
     // Utilizado ao atualizar grupos de um usuário
     static verificaPodeAtribuirGrupos(usuarioGrupos, gruposAtribuir) {
-        if(!usuarioGrupos || !(usuarioGrupos.length > 0)) {
+        if (!usuarioGrupos || !(usuarioGrupos.length > 0)) {
             usuarioGrupos = [GRUPO.VISITANTE];
         }
 
-        if(!GrupoService.possuiPermissao(usuarioGrupos, PERM.GRUPO, ACAO.ATRIBUIR)) {
+        if (!GrupoService.possuiPermissao(usuarioGrupos, PERM.GRUPO, ACAO.ATRIBUIR)) {
             return "Você não tem permissão para atribuir permissões de grupos a usuários.";
         }
 
         let podeAtribuirQualquer = GrupoService.possuiPermissao(usuarioGrupos, PERM.GRUPO, ACAO.ATRIBUIR, { qualquer_grupo: true });
         let usuarioMaiorNivel = 0;
         usuarioGrupos.forEach(g => {
-            if(grupos[g]._nivel > usuarioMaiorNivel) {
+            if (grupos[g]._nivel > usuarioMaiorNivel) {
                 usuarioMaiorNivel = grupos[g]._nivel;
             }
         });
 
-        for(let g of gruposAtribuir) {
+        for (let g of gruposAtribuir) {
             let grupo = grupos[g];
 
-            if(!grupo || grupo._oculto) {
-                return "Não é possível atribuir o grupo "+g+", pois não existe";
+            if (!grupo || grupo._oculto) {
+                return "Não é possível atribuir o grupo " + g + ", pois não existe";
             }
 
-            if(!podeAtribuirQualquer && grupo._nivel >= usuarioMaiorNivel) {
-                return "Você não tem permissão para atribuir o grupo "+g+" a usuários.";
+            if (!podeAtribuirQualquer && grupo._nivel >= usuarioMaiorNivel) {
+                return "Você não tem permissão para atribuir o grupo " + g + " a usuários.";
             }
         }
 
@@ -174,7 +174,7 @@ export default class GrupoService {
         const auth = [];
         for (const nomeGrupo of Object.keys(grupos)) {
             let grupo = grupos[nomeGrupo];
-            if(grupo._oculto) continue;
+            if (grupo._oculto) continue;
             for (const nomePermissao of Object.keys(grupo)) {
                 let permissao = grupo[nomePermissao];
                 for (const nomeAcao of Object.keys(permissao)) {
@@ -185,7 +185,7 @@ export default class GrupoService {
         return auth;
     }
 
-    static possuiPermissaoQualquerUsuario(req, usuarioRecurso, qualPermissao, qualAcao = false) { 
+    static possuiPermissaoQualquerUsuario(req, usuarioRecurso, qualPermissao, qualAcao = false) {
         // Se é o mesmo usuário ou o recurso não possui usuário, retorna verdadeiro
         let mesmoUsuario = !usuarioRecurso || (usuarioRecurso._id ? usuarioRecurso._id : usuarioRecurso).toString() === req.decodedToken._id;
         // Se possui a permissão de ver qualquer usuário desta permissão/ação, retorna verdadeiro
@@ -194,14 +194,14 @@ export default class GrupoService {
         return mesmoUsuario || podeQualquerUsuario;
     }
 
-    
-    static possuiPermissaoQualquerSecretaria(req, secretariaRecurso, qualPermissao, qualAcao = false) {
+
+    static possuiPermissaoQualquerCampus(req, campusRecurso, qualPermissao, qualAcao = false) {
         // Se é a mesma secretaria ou o recurso não possui secretaria, retorna verdadeiro
-        let mesmaSecretaria = !secretariaRecurso || (secretariaRecurso._id ? secretariaRecurso._id : secretariaRecurso).toString() === req.decodedToken.secretaria;
+        let mesmoCampus = !secretariaRecurso || (secretariaRecurso._id ? secretariaRecurso._id : secretariaRecurso).toString() === req.decodedToken.secretaria;
         // Se possui a permissão de ver qualquer secretaria desta permissão/ação, retorna verdadeiro
         let podeQualquerSecretaria = GrupoService.possuiPermissao(req.decodedToken.grupos, qualPermissao, qualAcao, { qualquer_secretaria: true });
 
-        return mesmaSecretaria || podeQualquerSecretaria;
+        return mesmoCampus || podeQualquerSecretaria;
     }
 
     /**
@@ -230,9 +230,15 @@ export default class GrupoService {
                 return sendError(res, 403, permissaoRecurso);
             }
      */
-    static possuiPermissaoRecurso(req, nomeRecurso, usuarioRecurso, qualPermissao, qualAcao = false) {
+    static possuiPermissaoRecurso(req, nomeRecurso, campusRecurso, usuarioRecurso, qualPermissao, qualAcao = false) {
 
-        if(usuarioRecurso) {
+        if (campusRecurso) {
+            if (!GrupoService.possuiPermissaoQualquerCampus(req, campusRecurso, qualPermissao, qualAcao)) {
+                return `Você não tem permissão para manipular um ${nomeRecurso} que não é de seu campus`;
+            }
+        }
+
+        if (usuarioRecurso) {
             if (!GrupoService.possuiPermissaoQualquerUsuario(req, usuarioRecurso, qualPermissao, qualAcao)) {
                 return `Você não tem permissão para manipular um ${nomeRecurso} que não é seu`;
             }
